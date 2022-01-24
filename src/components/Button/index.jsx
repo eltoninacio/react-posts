0609168f8cdbf93b9f6f7b3960abebe7f13a02bb
Elton Inacio <elton.inacio95@gmail.com>
2022-01-24 07:03:23 -0300

    convert component class to function

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,20 +1,13 @@
 import './styles.css';
 
-import { Component } from "react";
-
-export class Button extends Component {
-
-    render() {
-        const { text, onClick, disabled } = this.props;
-
-        return (
-            <button
-                disabled={disabled}
-                className='button'
-                onClick={onClick}
-            >
-                {text}
-            </button>
-        );
-    }
+export const Button = ({ text, onClick, disabled }) => {
+    return (
+        <button
+            disabled={disabled}
+            className='button'
+            onClick={onClick}
+        >
+            {text}
+        </button>
+    );
 }
